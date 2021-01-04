348ed47a40a375ec152f689291a7bebfa4ddb22b
Anmol Sinha <sinhaanmol.16@gmail.com>
2021-01-04 14:50:05 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/anmolsinha/radioify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anmolsinha.radioify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:radioify/utils/ai_util.dart';
+
+import 'package:velocity_x/velocity_x.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+{
+
+  @override
+  void initState() {
+
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      body: Stack(
+        children: [
+          VxAnimatedBox().size(context.screenWidth, context.screenHeight)
+              .withGradient(LinearGradient(
+            colors: [
+              AIColors.primaryColor1,
+              AIColors.primaryColor2,
+            ],begin: Alignment.topLeft,end: Alignment.bottomRight
+          ))
+              .make(),
+          AppBar(
+            title: "Radioify".text.xl4.bold.white.make().shimmer(primaryColor: Vx.purple300,secondaryColor: Colors.white),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            centerTitle: true,
+          ).h(100).p16()
+        ],
+      ),
+    );
+  }
+}
